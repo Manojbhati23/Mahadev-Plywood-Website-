ac6918503d040d940f852ac02f11068b6b9182f0
Manojbhati23 <rawatandsonstechno@gmail.com>
2025-03-02 00:11:26 +0530

    Add files via upload

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mahadev-plywood-hardware' title`, () => {
+  it(`should have the 'ManojPortfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mahadev-plywood-hardware');
+    expect(app.title).toEqual('ManojPortfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mahadev-plywood-hardware');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ManojPortfolio');
   });
 });
